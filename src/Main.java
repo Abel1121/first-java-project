a2f181f4cabc6fef35c735588df009d16a8e2fd3
Marek Kobylinski <mkobylinski@gwo.pl>
2024-02-22 14:09:21 +0100

    mathematical operators

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
+//        loop();
+//        variableTypes();
+        MathOperator.mathOperator();
+    }
+    private static void loop() {
         System.out.println("Hello and welcome!");
 
         for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+
             System.out.println("i = " + i);
         }
-
-        variableTypes();
     }
+
     private static void variableTypes() {
         byte byteVar = 1;
         System.out.println("byte, range -128 to 127, size 1 byte: " + byteVar);
