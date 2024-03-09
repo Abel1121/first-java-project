90fbd7ad91fd5be213e556fb07e95bf3278305ea
Marek Kobylinski <mkobylinski@gwo.pl>
2024-03-09 21:13:43 +0100

    static in java

--- src/Interface/Animal.java
+++ src/Interface/Animal.java
@@ -13,4 +13,8 @@ public interface Animal {
     default void wakeUp() {
         System.out.println("Woke UP!");
     }
+
+    static void log() { // you cant override static void like static variable
+        System.out.println("This is an animal interface!");
+    }
 }
--- src/Interface/Interface.java
+++ src/Interface/Interface.java
@@ -5,13 +5,20 @@ public class Interface {
         Cat cat = new Cat();
         Bird bird = new Bird();
         Dog dog = new Dog();
-
-        Animal[] animals = {cat, bird, dog};
-
-        for (Animal animal : animals) {
-            showAnimalInfo(animal);
-            System.out.println("------------------");
-        }
+        System.out.println(dog.getNumberOfLegs());
+        System.out.println(Dog.getDogsCount());
+        System.out.println(Dog.getAllDogsLegs());
+        Dog dog2 = new Dog();
+        System.out.println(dog2.getNumberOfLegs());
+        System.out.println(Dog.getDogsCount());
+        System.out.println(Dog.getAllDogsLegs());
+        Animal.log();
+//        Animal[] animals = {cat, bird, dog};
+//
+//        for (Animal animal : animals) {
+//            showAnimalInfo(animal);
+//            System.out.println("------------------");
+//        }
     }
 
     private static void showAnimalInfo(Animal animal) {
